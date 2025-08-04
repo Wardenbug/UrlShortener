baf9405feefabf887b3b902f3b6a6292956b93c7
Wardenbug <danylo.storozhuk.ua@gmail.com>
2025-08-04 12:19:50 +0200

    chore: project initialization

---
+++ src/UrlShortener.Api/Migrations/202508041214_CreateUrlTable.cs
@@ -0,0 +1,28 @@
+﻿// Migrations/202508041214_CreateUrlTable.cs
+using FluentMigrator;
+
+namespace UrlShortener.Api.Migrations;
+
+[Migration(202508041214)]
+internal sealed class CreateUrlTable : Migration
+{
+    public override void Up()
+    {
+        Create.Table("urls")
+            .WithColumn("id")
+                .AsInt32()
+                .PrimaryKey()
+                .Identity()
+            .WithColumn("original_url")
+                .AsString()
+                .NotNullable()
+            .WithColumn("short_code")
+                .AsString()
+                .NotNullable();
+    }
+
+    public override void Down()
+    {
+        Delete.Table("urls");
+    }
+}
--- src/UrlShortener.Api/Program.cs
+++ src/UrlShortener.Api/Program.cs
@@ -1,13 +1,28 @@
+using FluentMigrator.Runner;
+using UrlShortener.Api.Migrations;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddFluentMigratorCore()
+    .ConfigureRunner(rb => rb
+        .AddPostgres()
+        .WithGlobalConnectionString(builder.Configuration.GetConnectionString("Database"))
+        .ScanIn(typeof(CreateUrlTable).Assembly).For.Migrations())
+    .AddLogging(lb => lb.AddFluentMigratorConsole());
 
 WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+}
+
+if (builder.Configuration.GetValue<bool>("RunMigrations"))
+{
+    using var scope = app.Services.CreateScope();
+    var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+    runner.MigrateUp();
 }
 
 app.UseHttpsRedirection();
