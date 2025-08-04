5c3383c066ec9f763aa577e2c632f4d42fe49e93
Wardenbug <danylo.storozhuk.ua@gmail.com>
2025-08-04 11:59:12 +0200

    initial commit

---
+++ src/UrlShortener.Api/Program.cs
@@ -0,0 +1,15 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+await app.RunAsync().ConfigureAwait(false);
